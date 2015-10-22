71e9337f27ac3b47dab82e1abbd03685d214de4b
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-22 03:53:34 +0000

    use invokables in serviceManager

--- module/Guestbook/Module.php
+++ module/Guestbook/Module.php
@@ -5,7 +5,6 @@ use Guestbook\Model\Entry;
 use Zend\Db\ResultSet\ResultSet;
 use Guestbook\Model\EntryTable;
 use Guestbook\Form\Entry as EntryForm;
-use Guestbook\Form\EntryFilter; 
 
 class Module
 {
@@ -28,18 +27,21 @@ class Module
     public function getServiceConfig()
     {
         return array(
+            'invokables' => array(
+                'guestbook_model_entry' => 'Guestbook\Model\Entry',
+                'guestbook_entry_filter' => 'Guestbook\Form\EntryFilter',  
+            ),
             'factories' => array(
                 'guestbook_entry_table' =>  function($sm) {
                     $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
                     $resultSetPrototype = new ResultSet();
-                    $resultSetPrototype->setArrayObjectPrototype(new Entry());
+                    $resultSetPrototype->setArrayObjectPrototype($sm->get('guestbook_model_entry'));
                     $entryTable =  new EntryTable('guestbook_entry', $dbAdapter, null, $resultSetPrototype);
                     return $entryTable;
                 },
                 'guestbook_form' => function($sm) {
                     $entryForm = new EntryForm();
-                    $filter = new EntryFilter();
-                    $entryForm->setInputFilter($filter);
+                    $entryForm->setInputFilter($sm->get('guestbook_entry_filter'));
                     return $entryForm;
                 }
             ),
