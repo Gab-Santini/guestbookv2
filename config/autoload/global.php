b7ed71848bcaf5f6acff82a6902bc5617edbe303
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-22 01:28:23 +0000

    create connection with database

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,5 +12,12 @@
  */
 
 return array(
-    // ...
+    'db' => array(
+        'driver'         => 'pdo',
+        'dsn'            => 'mysql:dbname=guestbook;host=localhost',
+        'username'       => 'root',
+        'password'       => 'root',
+        'driver_options' => array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+        						  PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION),
+    ),
 );
