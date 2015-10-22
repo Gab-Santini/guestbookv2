9bb18bf3fc0f9cdbc162a330775644cbe8d023f0
Walid MNASRI <mnasri.walid@gmail.com>
2015-10-22 02:31:40 +0000

    use ServiceManager to configure the tableGateway

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -12,6 +12,12 @@
  */
 
 return array(
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter'
+            => 'Zend\Db\Adapter\AdapterServiceFactory',
+        ),
+    ),
     'db' => array(
         'driver'         => 'pdo',
         'dsn'            => 'mysql:dbname=guestbook;host=localhost',
--- module/Guestbook/Module.php
+++ module/Guestbook/Module.php
@@ -1,6 +1,9 @@
 <?php
 namespace Guestbook;
 
+use Guestbook\Model\Entry;
+use Zend\Db\ResultSet\ResultSet;
+use Guestbook\Model\EntryTable;
 class Module
 {
     public function getConfig()
@@ -15,6 +18,21 @@ class Module
                 'namespaces' => array(
                     __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
                 ),
+            ),
+        );
+    }
+    
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'guestbook_entry_table' =>  function($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Entry());
+                    $entryTable =  new EntryTable('guestbook_entry', $dbAdapter, null, $resultSetPrototype);
+                    return $entryTable;
+                }
             ),
         );
     }
